022bae8699186b60a8f8c40c3f5a3bc5a6fe1d77
Harsh2432 <jadhavharsh243@gmail.com>
2022-04-26 16:18:00 +0530

    Edit style.css

--- sketch.js
+++ sketch.js
@@ -14,3 +14,44 @@ function changeColor() {
 }
 
 setInterval(changeColor, 1000);
+
+let options = document.querySelector(".options");
+
+const customColor = document.createElement("button");
+customColor.classList.add("customColor");
+customColor.textContent = "Color Mode";
+customColor.style.cssText = "color: black; justify-content: center; align-items: center; font-family: monospace; font-size: 18px; font-weight: bold; padding: 10px; border-radius: 10px; height: 50px; width: 200px;";
+customColor.style.backgroundColor = getRandomColor();
+
+customColor.addEventListener("click", () => {
+    customColor.style.backgroundColor = getRandomColor();
+});
+
+options.appendChild(customColor);
+
+const rainbowColor = document.createElement("button");
+rainbowColor.classList.add("rainbowColor");
+rainbowColor.textContent = "Rainbow Mode";
+rainbowColor.style.cssText = "color: black; justify-content: center; align-items: center; font-family: monospace; font-size: 18px; font-weight: bold; padding: 10px; border-radius: 10px; height: 50px; width: 200px;";
+
+function rainbowButtonColor() {
+    rainbowColor.style.backgroundColor = getRandomColor();
+}
+
+setInterval(rainbowButtonColor, 1000);
+
+options.appendChild(rainbowColor);
+
+const eraser = document.createElement("button");
+eraser.classList.add("eraser");
+eraser.textContent = "Eraser";
+eraser.style.cssText = "color: black; justify-content: center; align-items: center; font-family: monospace; font-size: 18px; font-weight: bold; padding: 10px; border-radius: 10px; height: 50px; width: 200px;";
+
+options.appendChild(eraser);
+
+const clear = document.createElement("button");
+clear.classList.add("clear");
+clear.textContent = "Clear";
+clear.style.cssText = "color: black; justify-content: center; align-items: center; font-family: monospace; font-size: 18px; font-weight: bold; padding: 10px; border-radius: 10px; height: 50px; width: 200px;";
+
+options.appendChild(clear);
